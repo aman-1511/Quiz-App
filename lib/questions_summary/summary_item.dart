11fc04395c10e6085550d57fed929e6690606dbe
Aman <amanchaudharyjaat12345@gmail.com>
2024-03-01 01:02:27 +0530

    first commit

---
+++ lib/questions_summary/summary_item.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:quiz_app/questions_summary/question_identifier.dart';
+
+class SummaryItem extends StatelessWidget {
+  const SummaryItem(this.itemData, {super.key});
+
+  final Map<String, Object> itemData;
+
+  @override
+  Widget build(BuildContext context) {
+    final isCorrectAnswer =
+        itemData['user_answer'] == itemData['correct_answer'];
+
+    int questionIndex;
+    if (itemData['question'] is int) {
+      questionIndex = itemData['question'] as int;
+    } else if (itemData['question'] is String) {
+      questionIndex = int.tryParse(itemData['question'] as String) ?? 0;
+    } else {
+      questionIndex = 0;
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 8,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          QuestionIdentifier(
+            isCorrectAnswer: isCorrectAnswer,
+            questionIndex: questionIndex,
+          ),
+          const SizedBox(width: 20),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  itemData['question'].toString(), // Convert to string
+                  style: GoogleFonts.lato(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(itemData['user_answer'].toString(),
+                    style: const TextStyle(
+                      color: Color.fromARGB(255, 202, 171, 252),
+                    )),
+                Text(itemData['correct_answer'].toString(),
+                    style: const TextStyle(
+                      color: Color.fromARGB(255, 181, 254, 246),
+                    )),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
